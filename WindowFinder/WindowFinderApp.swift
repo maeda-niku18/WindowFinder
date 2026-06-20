789dff0578bbf0234acd9dae7abc194254ea8d15
niku-18 <joqran1221@gmail.com>
2026-06-20 12:31:53 +0900

    Initial Commit

---
+++ WindowFinder/WindowFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowFinderApp.swift
+//  WindowFinder
+//
+//  Created by 前田悟志 on 2026/06/20.
+//
+
+import SwiftUI
+
+@main
+struct WindowFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
